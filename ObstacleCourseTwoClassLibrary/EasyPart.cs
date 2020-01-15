eb9a32de89adc05e702e54a5977b3199932e0a10
Alex <akotov@gmail.com>
2020-01-14 23:49:48 -0800

    added EasyPart.cs

---
+++ ObstacleCourseTwoClassLibrary/EasyPart.cs
@@ -0,0 +1,49 @@
+﻿namespace ObstacleCourseTwoClassLibrary
+{
+    public static class EasyPart
+    {
+
+        // create array with 5 elements and fill it with 1's - no loops
+        public static int[] CreateArray5ElementsAndFillWithOnesNoLoop()
+        {
+            int[] result = new int[5];
+            result[0] = 1;
+            result[1] = 1;
+            result[2] = 1;
+            result[3] = 1;
+            result[4] = 1;
+            return result;
+        }
+
+        // create array with 5 elements and fill it with 1's - for loop
+        public static int[] CreateArray5ElementsAndFillWithOnesForLoop()
+        {
+            int[] result = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                result[i] = 1;
+            }
+            return result;
+        }
+
+        // create array with N elements and fill it with 2's - for loop
+        public static int[] CreateArrayNElementsAndFillWithTwosForLoop(int n)
+        {
+            int[] result = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                result[i] = 2;
+            }
+            return result;
+        }
+
+        // create array with N elements and fill it with 3's - for loop
+        public static int[] CreateArrayNElementsAndFillWithThreesForLoop(int n)
+        {
+            int[] result = new int[n];
+            // write code here
+            return result;
+        }
+
+    }
+}
---
+++ ObstacleCourseTwoUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ObstacleCourseTwoUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ObstacleCourseTwoUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("47767fb1-0a97-4312-b10a-1409be6fda0a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
